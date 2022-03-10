23a35fa10bfea2cdddf30ce80a6f361f776540f5
Shreyas <shreyastamboli1111@gmail.com>
2022-03-10 10:02:06 +0530

    Excute

---
+++ WebMvc/WebMvc/Controllers/ViewController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvc.Controllers
+{
+    [Route("View")]
+    public class ViewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("Get")]
+        public IActionResult Get()
+        {
+            return Content("Welcome To Web MVC");
+        }
+        [HttpPost]
+        [Route("Post")]
+        public IActionResult Post()
+        {
+            return Content("Welcome to Post");
+        }
+    }
+}
