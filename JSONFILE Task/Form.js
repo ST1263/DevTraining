656e101ebadc10eb1ae6b30489ede8752e51d028
Shreyas <shreyastamboli1111@gmail.com>
2022-03-24 00:05:55 +0530

    Form Typescript

---
+++ JSONFILE Task/Form.js
@@ -0,0 +1,10 @@
+//let mix = {age:10,city:"Pune"}
+//mix.state = "Maharastra";
+//mix["state"]="maharsatra";
+//console.log(mix);
+function Check() {
+    var inputValue = document.getElementById("fname").value;
+    var inputphone = document.getElementById("pno").value;
+    console.log(inputValue);
+    console.log(inputphone);
+}
---
+++ JSONFILE Task/Form.ts
@@ -0,0 +1,17 @@
+//let mix = {age:10,city:"Pune"}
+//mix.state = "Maharastra";
+//mix["state"]="maharsatra";
+//console.log(mix);
+
+
+function Check(){      
+      var inputValue: string= (<HTMLInputElement>document.getElementById("fname")).value;
+var inputphone= (<HTMLInputElement>document.getElementById("pno")).value;
+console.log(inputValue)
+console.log(inputphone)
+
+}
+
+
+
+
