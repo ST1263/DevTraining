596e87644fd129fcb524af69c6a1c2decbef9e59
Shreyas <shreyastamboli1111@gmail.com>
2022-04-14 07:20:58 +0530

    Crud Done

--- HostelManagementSystem/HostelManagementSystem/Repo/AdmissionRepo.cs
+++ HostelManagementSystem/HostelManagementSystem/Repo/AdmissionRepo.cs
@@ -21,31 +21,57 @@ namespace HostelManagementSystem.Repo
         public List<AdmissionVM> GetAdmissions()
         {
             //return _context.Admission.ToList();
-            var result = (from a in _context.Admission
-                          select new AdmissionVM
-                          {
-                              AdmissionId = a.AdmissionId,
-                              FullName = a.FullName,
-                              EmailId = a.EmailId,
-                              MobileNo= a.MobileNo,
-                              DateOfBirth = a.DateOfBirth,
-                              BloodGroup = a.BloodGroup,
-                              Caste = a.Caste,
-                              GuardianName = a.GuardianName,
-                              GuardianEmailId = a.GuardianEmailId,
-                              GuardianMobileNo = a.GuardianMobileNo,
-                              CourseName = a.CourseName,
-                              CurrentYear = a.CurrentYear,
-                              CollegeName = a.CollegeName,
-                              HouseName = a.HouseName,
-                              Locality = a.Locality,
-                              City = a.City,
-                              State = a.State,
-                              Country = a.Country,
-                              ZipCode = a.ZipCode,
-                              createdAT = a.createdAT,
-                              createdBy = a.createdBy,
-                          }).ToList();
+            //var result = (from a in _context.Admission
+            //              select new AdmissionVM
+            //              {
+            //                  AdmissionId = a.AdmissionId,
+            //                  FullName = a.FullName,
+            //                  EmailId = a.EmailId,
+            //                  MobileNo= a.MobileNo,
+            //                  DateOfBirth = a.DateOfBirth,
+            //                  BloodGroup = a.BloodGroup,
+            //                  Caste = a.Caste,
+            //                  GuardianName = a.GuardianName,
+            //                  GuardianEmailId = a.GuardianEmailId,
+            //                  GuardianMobileNo = a.GuardianMobileNo,
+            //                  CourseName = a.CourseName,
+            //                  CurrentYear = a.CurrentYear,
+            //                  CollegeName = a.CollegeName,
+            //                  HouseName = a.HouseName,
+            //                  Locality = a.Locality,
+            //                  City = a.City,
+            //                  State = a.State,
+            //                  Country = a.Country,
+            //                  ZipCode = a.ZipCode,
+            //                  createdAT = a.createdAT,
+            //                  createdBy = a.createdBy,
+            //                 RoomAllocationNo = a.RoomAllocation.RoomAllocationNo
+            //              }).ToList();
+            var result = _context.Admission.Select(a => new AdmissionVM
+            {
+                AdmissionId = a.AdmissionId,
+                FullName = a.FullName,
+                EmailId = a.EmailId,
+                MobileNo = a.MobileNo,
+                DateOfBirth = a.DateOfBirth,
+                BloodGroup = a.BloodGroup,
+                Caste = a.Caste,
+                GuardianName = a.GuardianName,
+                GuardianEmailId = a.GuardianEmailId,
+                GuardianMobileNo = a.GuardianMobileNo,
+                CourseName = a.CourseName,
+                CurrentYear = a.CurrentYear,
+                CollegeName = a.CollegeName,
+                HouseName = a.HouseName,
+                Locality = a.Locality,
+                City = a.City,
+                State = a.State,
+                Country = a.Country,
+                ZipCode = a.ZipCode,
+                createdAT = a.createdAT,
+                createdBy = a.createdBy,
+                RoomAllocationNo = a.RoomAllocation.RoomAllocationNo
+            }).ToList();
             return result;
         }
 
@@ -108,6 +134,8 @@ namespace HostelManagementSystem.Repo
 
         public void AddAdmission(Admission admission)
         {
+           // admission.createdAT = DateTime.Now;
+            //admission.createdBy = 
             _context.Add(admission);
             _context.SaveChanges();
         }
