984b4bf54629f32f1b451b3783873a4462c50581
Shreyas <shreyastamboli1111@gmail.com>
2022-04-24 00:56:20 +0530

    Done All Crud

---
+++ ProjectNS/ProjectNS/ViewModel/FDAClassificationVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectNS.ViewModel
+{
+    public class FDAClassificationVM
+    {
+        public int FDAClassificationId { get; set; }
+        public string FdaClassificationName { get; set; }
+        public string Description { get; set; }
+    }
+}
